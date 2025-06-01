4689eec3c05dbefa0c94058c0b463712a90e036c
Ian Ramy <imwagore@gmail.com>
2025-06-01 21:05:35 +0300

    Updated models.py in chats/

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=10, blank=True)
+    password = models.CharField(max_length=128)
 
-    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+    REQUIRED_FIELDS = ["email", "first_name", "last_name", "password"]
     USERNAME_FIELD = "username"
 
     def __str__(self):
