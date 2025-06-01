713c219c61446f8152537000d18016d212d7920f
Ian Ramy <imwagore@gmail.com>
2025-06-01 20:49:45 +0300

    Updated urls & views.py in chats/

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
-from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 
-convo_router = NestedDefaultRouter(router, r"conversations", lookup="conversation")
-convo_router.register(r"messages", MessageViewSet, basename="conversation-messages")
+nested_router = NestedDefaultRouter(router, r"conversations", lookup="conversation")
+nested_router.register(r"messages", MessageViewSet, basename="conversation-messages")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("", include(convo_router.urls)),
+    path("", include(nested_router.urls)),
 ]
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status # noqa
+from django_filters import rest_framework as filters
+from rest_framework import viewsets, filters as drf_filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -11,6 +13,9 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.DjangoFilterBackend, drf_filters.OrderingFilter]
+    filterset_fields = ["conversation"]
+    ordering_fields = ["timestamp"]
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
