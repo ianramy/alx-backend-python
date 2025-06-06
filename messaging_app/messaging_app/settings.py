806b469ac4fc49705dffe1e1eb56246e7a2f5350
Ian Ramy <imwagore@gmail.com>
2025-06-06 17:03:59 +0300

    Updated Settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -141,6 +141,7 @@ REST_FRAMEWORK.update(
     {
         "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
         "PageNumberPagination": 20,
+        "PAGE_SIZE": 20,
         "DEFAULT_FILTER_BACKENDS": [
             "django_filters.rest_framework.DjangoFilterBackend"
         ],
